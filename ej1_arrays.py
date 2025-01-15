a3fe8912e0eaf611e4e5a515a169244532537f73
rodgar00 <rodrigogarciaperez00@gmail.com>
2025-01-15 11:12:12 +0100

    Inicio vector

--- ej1_arrays.py
+++ ej1_arrays.py
@@ -1,1 +1,5 @@
-##Realizar programa Java que permita cargar un vector numérico de 10 elementos desde teclado y, posteriormente visualice el valor del elemento mayor y cuántas veces se repite en el vector este valor máximo. Utiliza JOptionPane
+#Realizar programa Java que permita cargar un vector numérico de 10 elementos desde teclado y, posteriormente visualice el valor del elemento mayor y cuántas veces se repite en el vector este valor máximo. Utiliza JOptionPane
+vector = []
+
+
+
