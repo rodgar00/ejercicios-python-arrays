4d60b12a2a094dc485e72fedceb159df1c4c106f
rodgar00 <rodrigogarciaperez00@gmail.com>
2025-01-16 12:30:40 +0100

    ej1 completo

--- ej1_arrays.py
+++ ej1_arrays.py
@@ -1,5 +1,12 @@
 #Realizar programa Java que permita cargar un vector numérico de 10 elementos desde teclado y, posteriormente visualice el valor del elemento mayor y cuántas veces se repite en el vector este valor máximo. Utiliza JOptionPane
 vector = []
 
+for i in range (10):
+    numero = float(input(f"Dime un número"))
+    vector.append(numero)
 
+numMax = max(vector)
 
+repeticion = vector.count(numMax)
+
+print(f"El número mayor es: {numMax} y se repite {repeticion} veces")
